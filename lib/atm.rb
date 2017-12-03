830a2b5c886b81d9a252fa30e39c488a992feb24
ozeron <ozeron@me.com>
2017-12-03 14:25:53 +0200

    ADDED: Ability to withdraw money

--- lib/atm.rb
+++ lib/atm.rb
@@ -22,7 +22,37 @@ class Atm
     end
   end
 
+  def withdraw(amount)
+    fixed_amount = amount.to_i
+    validate_amount!(fixed_amount)
+    result = NOMINALS.reverse.each_with_object({}) do |nominal, memo|
+      needed = fixed_amount / nominal
+      will_take = [state.fetch(nominal, 0), needed].min
+      next if will_take.zero?
+      state[nominal] -= will_take
+      memo[nominal] = will_take
+      fixed_amount -= nominal * will_take
+    end
+    return result if fixed_amount.zero?
+    raise ArgumentError, "Can not withdraw sum: #{amount}. Have only nominals: #{state.keys.join(', ')}"
+  end
+
   private
+
+  def validate_amount!(amount)
+    validate_amount_bigger_than_zero!(amount)
+    validate_amount_less_than_max_withdraw!(amount)
+  end
+
+  def validate_amount_bigger_than_zero!(amount)
+    return if amount >= 0
+    raise ArgumentError, 'Amount should be positive number'
+  end
+
+  def validate_amount_less_than_max_withdraw!(amount)
+    return if amount < max_withdraw
+    raise ArgumentError, "Can not withdraw this sum. You ask for: #{amount}, max is #{max_withdraw}"
+  end
 
   def validate_hash!(hash)
     validate_nominal_quanity!(hash)
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -80,4 +80,73 @@ describe Atm do
       end
     end
   end
+
+  describe '#withdraw' do
+    subject { atm.withdraw(amount) }
+
+    let(:state) { { 50 => 2 } }
+    let(:atm) { described_class.new(state) }
+    let(:amount) { 50 }
+    let(:result) { { 50 => 1 } }
+
+    it 'return hash with nominals and quanity' do
+      is_expected.to eq(result)
+    end
+
+    context 'when received like in task' do
+      let(:state) { { 50 => 3, 25 => 4 } }
+      let(:result) { { 50 => 3, 25 => 2 } }
+      let(:amount) { 200 }
+
+      it { is_expected.to eq(result) }
+    end
+
+    context 'when amount is complex' do
+      let(:state) { { 50 => 2, 25 => 1, 10 => 3, 2 => 3, 1 => 1 } }
+      let(:result) { { 50 => 1, 25 => 1, 10 => 1, 2 => 2, 1 => 1 } }
+      let(:amount) { 90 }
+
+      it { is_expected.to eq(result) }
+    end
+
+    context 'when amount is bigger than max_withdraw' do
+      let(:amount) { 500 }
+
+      it 'raise ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when amount is less than zero' do
+      let(:amount) { -50 }
+
+      it 'raise ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when amount can not be given' do
+      let(:amount) { 60 }
+
+      it 'return hash with nominals and quanity' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when amount can not be converted to int' do
+      let(:amount) { 14.3 }
+
+      it 'return hash with nominals and quanity' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when amount can be converted to int' do
+      let(:amount) { 50.0 }
+
+      it 'return hash with nominals and quanity' do
+        is_expected.to eq(result)
+      end
+    end
+  end
 end
